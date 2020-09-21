1e3f5430382e12dab831330a0a5b51bae1ebf9b7
Undefined-user <undefinded-user@users.noreply.github.com>
2020-09-21 23:48:32 +0300

    create app-component
    -fetch todos data
    -create changeHandler
    -map todos data into TodoComponents

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Todo (props) {
 	return (
 		<div className="todo-item">
-			<input type="checkbox" checked={props.data.completed} onChange={() => props.handleChange} />
+			<input 
+				type="checkbox" checked={props.data.completed} 
+				onChange={() => props.changeHandler(props.data.id)} 
+			/>
 			<p>{props.data.title}</p>
 		</div>
 	)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Todo from './components/Todo.js'
 
 
+class MyApp extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			todos: []
+		}
+		this.state.createTodoComponents = this.createTodoComponents.bind(this)
+		this.changeHandler = this.changeHandler.bind(this)
+	}
 
-function MyApp () {
-	return (
-		<>
-			<h1>Hellow everyone</h1>
-			<p>This message is from React</p>
-			<p>My first functional component</p>
-		</>
+	componentDidMount(){
+		fetch("https://jsonplaceholder.typicode.com/todos/")
+			.then(res => res.json())
+				.then(data => this.setState({
+					todos: data
+				}))
+		
+	}
+
+	changeHandler(id){
+		const updatedTodos = this.state.todos.map( (todoItem) => {
+			if(id === todoItem.id) return {
+				...todoItem,
+				completed: !todoItem.completed
+			}
+			return todoItem
+		})
+		this.setState({
+			todos: updatedTodos
+		})
+	}
+
+	createTodoComponents(){
+		const todosComponents = this.state.todos.map( todoData => {
+			return <Todo 
+						data={todoData} 
+						changeHandler={this.changeHandler}
+						key={todoData.id}
+					/>
+		})
+
+		return todosComponents
+	}
+
+	render(){
+		
+		return(
+			<div>
+				{this.createTodoComponents()}
+			</div>
 		)
+	}
+
 }
 
 ReactDOM.render(<MyApp /> , document.getElementById("root"))
