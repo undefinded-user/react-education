c963c845686d1b7980186977a2c3d7d6b9c9f912
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-18 12:55:59 +0300

    Revert "Create controlled component"
    This reverts commit 7403e067fe6c1af6847e984af9fd0133f20d4b7c.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,16 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const CharacterCounterInput = ({ text, defaults }) => {
-  const [message, setMessage] = useState('')
-  const maxLength = 20
-  
-  return <div className={`counterInput ${message.length > maxLength ? "tooLong" : ""}`}>
-    <div>
-      {defaults.map((b) => {
-        return <button key={b} onClick={() => {
-          setMessage(b)
-        }}>{b}</button>
-      })}
-    </div>
-    <textarea
-      placeholder={text}
-      value={message}
-      onChange={(event) => {
-        setMessage(event.target.value)
-      }}
-    />
-    <div>{message.length}/{maxLength}</div>
-  </div>
+
+
+function MyApp () {
+	return (
+		<>
+			<h1>Hellow everyone</h1>
+			<p>This message is from React</p>
+			<p>My first functional component</p>
+		</>
+		)
 }
 
-const App = () => {
-  let defaultMoods = ["Great", "Okay", "Bad", "TERRIBLE"]
-  
-  return (
-    <section>
-      <h2>Mood Tracker</h2>
-      <CharacterCounterInput text={"How was your day?"} defaults={defaultMoods}/>
-    </section>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyApp /> , document.getElementById("root"))
