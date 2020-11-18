7403e067fe6c1af6847e984af9fd0133f20d4b7c
Undefined-user <undefinded-user@users.noreply.github.com>
2020-11-18 12:54:54 +0300

    Create controlled component
    -Use react hooks
    -Controlled component allows us to manipulate different parts of DOM when state is changed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-
-
-function MyApp () {
-	return (
-		<>
-			<h1>Hellow everyone</h1>
-			<p>This message is from React</p>
-			<p>My first functional component</p>
-		</>
-		)
+const CharacterCounterInput = ({ text, defaults }) => {
+  const [message, setMessage] = useState('')
+  const maxLength = 20
+  
+  return <div className={`counterInput ${message.length > maxLength ? "tooLong" : ""}`}>
+    <div>
+      {defaults.map((b) => {
+        return <button key={b} onClick={() => {
+          setMessage(b)
+        }}>{b}</button>
+      })}
+    </div>
+    <textarea
+      placeholder={text}
+      value={message}
+      onChange={(event) => {
+        setMessage(event.target.value)
+      }}
+    />
+    <div>{message.length}/{maxLength}</div>
+  </div>
 }
 
-ReactDOM.render(<MyApp /> , document.getElementById("root"))
+const App = () => {
+  let defaultMoods = ["Great", "Okay", "Bad", "TERRIBLE"]
+  
+  return (
+    <section>
+      <h2>Mood Tracker</h2>
+      <CharacterCounterInput text={"How was your day?"} defaults={defaultMoods}/>
+    </section>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
